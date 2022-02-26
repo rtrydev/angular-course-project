40a465dc3a4ebbb3e24358dad94971362487f9e8
rtrydev <r.kulka44@gmail.com>
2022-02-26 15:44:07 +0100

    passing ingredients

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -1,6 +1,7 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from "./recipes/recipe.model";
 import {Ingredient} from "./shared/ingredient.model";
+import {ShoppingListService} from "./shopping-list.service";
 
 @Injectable({
   providedIn: 'root'
@@ -24,5 +25,9 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
-  constructor() { }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+
+  constructor(private shoppingListService: ShoppingListService) { }
 }
--- src/app/shopping-list.service.ts
+++ src/app/shopping-list.service.ts
@@ -22,5 +22,10 @@ export class ShoppingListService {
     this.ingredientsChanged.emit(this.ingredients.slice());
   }
 
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
   constructor() { }
 }
